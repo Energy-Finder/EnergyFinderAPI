1ee6952e1ec57e968f7dfbfbaba86370fdc162b0
alessandro raul <alessandro.guedes@bandtec.com.br>
2022-01-26 20:40:41 -0300

    feat: add update provider methods and endpoints

--- src/controllers/ProviderController.js
+++ src/controllers/ProviderController.js
@@ -8,10 +8,32 @@ const responseModel = {
     error: []
 };
 
-const get = function(req, res, query) {
+const createOrUpdate = function (req, res, query) {
     let response = { ...responseModel };
     let statusCode = 500;
-    
+
+    const isInvalidRequest = validateRequestFields(req, res, response);
+    if (isInvalidRequest) return false;
+
+    sequelize.query(query, { model: tbProvider }
+    ).then(() => {
+        if (query.includes('INSERT')) {
+            statusCode = 201;
+        } else {
+            statusCode = 200;
+        }
+        response.success = true;
+    }).catch(err => {
+        response.error = err.message;
+    }).finally(() => {
+        return res.status(statusCode).json(response);
+    })
+}
+
+const get = function (req, res, query) {
+    let response = { ...responseModel };
+    let statusCode = 500;
+
     sequelize.query(`${query}`, { model: tbProvider }
     ).then(res => {
         statusCode = 200;
@@ -27,21 +49,23 @@ const get = function(req, res, query) {
 module.exports = {
     async create(req, res) {
         const { name, logo, uf, kwhPrice, kwhMinLimit, totalClients, averageRating } = req.body;
-        let response = { ...responseModel };
-        let statusCode = 500;
+        const query = `INSERT INTO tbProvider VALUES ('${name}', '${logo}', '${uf}', ${kwhPrice}, ${kwhMinLimit}, ${totalClients}, ${averageRating})`;
+        createOrUpdate(req, res, query);
+    },
 
-        const isInvalidRequest = validateRequestFields(req, res, response);
-        if (isInvalidRequest) return false;
-        
-        sequelize.query(`INSERT INTO tbProvider VALUES ('${name}', '${logo}', '${uf}', ${kwhPrice}, ${kwhMinLimit}, ${totalClients}, ${averageRating})`, { model: tbProvider }
-        ).then(() => {
-            statusCode = 201;
-            response.success = true;
-        }).catch(err => {
-            response.error = err.message;
-        }).finally(() => {
-            return res.status(statusCode).json(response);
-        })
+    async update(req, res) {
+        const id = req.params.id;
+        const { name, logo, uf, kwhPrice, kwhMinLimit, totalClients, averageRating } = req.body;
+        const query = `UPDATE tbProvider SET 
+        providerName='${name}', 
+        providerLogo='${logo}', 
+        providerUf='${uf}', 
+        providerKwhPrice=${kwhPrice}, 
+        providerKwhLimit=${kwhMinLimit}, 
+        providerClientsTotal=${totalClients}, 
+        providerAverageRating=${averageRating}
+        WHERE providerId=${id}`;
+        createOrUpdate(req, res, query);
     },
 
     async getAll(req, res) {
--- src/routes.js
+++ src/routes.js
@@ -1,29 +1,40 @@
 const router = require('express').Router();
-const { body } = require("express-validator");
+const { body, param } = require('express-validator');
 
 const UserController = require('./controllers/UserController');
 const ProviderController = require('./controllers/ProviderController');
 
 router.post('/user', [
-    body('username').notEmpty().withMessage(`username cannot be null`),
-    body('email').isEmail().withMessage(`email is invalid`),
-    body('password').isLength({ min: 3 }).withMessage(`password should be greater than 2 characters`)
+    body('username').notEmpty().withMessage('username cannot be null'),
+    body('email').isEmail().withMessage('email is invalid'),
+    body('password').isLength({ min: 3 }).withMessage('password should be greater than 2 characters')
 ], UserController.create);
 
 router.post('/user/auth', [
-    body('email').isEmail().withMessage(`email is invalid`),
-    body('password').isLength({ min: 3 }).withMessage(`password should be greater than 2 characters`)
+    body('email').isEmail().withMessage('email is invalid'),
+    body('password').isLength({ min: 3 }).withMessage('password should be greater than 2 characters')
 ], UserController.auth);
 
 router.post('/provider', [
-    body('name').notEmpty().withMessage(`name cannot be null`),
-    body('logo').notEmpty().withMessage(`logo cannot be null`),
-    body('uf').isLength({ max: 2 }).withMessage(`uf should be 2 characters`),
-    body('kwhPrice').isFloat().withMessage(`kwhPrice should be float type`),
-    body('kwhMinLimit').isFloat().withMessage(`kwhMinLimit should be float type`),
-    body('totalClients').isInt().withMessage(`kwhMinLimit should be integer type`),
-    body('averageRating').isFloat().withMessage(`kwhMinLimit should be float type`),
+    body('name').notEmpty().withMessage('name cannot be null'),
+    body('logo').notEmpty().withMessage('logo cannot be null'),
+    body('uf').isLength({ max: 2 }).withMessage('uf should be 2 characters'),
+    body('kwhPrice').isFloat().withMessage('kwhPrice should be float type'),
+    body('kwhMinLimit').isFloat().withMessage('kwhMinLimit should be float type'),
+    body('totalClients').isInt().withMessage('kwhMinLimit should be integer type'),
+    body('averageRating').isFloat().withMessage('kwhMinLimit should be float type'),
 ], ProviderController.create);
+
+router.put('/provider/:id', [
+    param('id').notEmpty().withMessage('query param "id" is required'),
+    body('name').notEmpty().withMessage('name cannot be null'),
+    body('logo').notEmpty().withMessage('logo cannot be null'),
+    body('uf').isLength({ max: 2 }).withMessage('uf should be 2 characters'),
+    body('kwhPrice').isFloat().withMessage('kwhPrice should be float type'),
+    body('kwhMinLimit').isFloat().withMessage('kwhMinLimit should be float type'),
+    body('totalClients').isInt().withMessage('kwhMinLimit should be integer type'),
+    body('averageRating').isFloat().withMessage('kwhMinLimit should be float type'),
+], ProviderController.update);
 
 router.get('/provider', ProviderController.getAll);
 
