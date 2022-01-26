dd4101b76377c64ab7ef296b23785dd43987f054
alessandro raul <alessandro.guedes@bandtec.com.br>
2022-01-26 20:02:31 -0300

    feat: add new methods in providerController

--- src/controllers/ProviderController.js
+++ src/controllers/ProviderController.js
@@ -8,16 +8,32 @@ const responseModel = {
     error: []
 };
 
+const get = function(req, res, query) {
+    let response = { ...responseModel };
+    let statusCode = 500;
+    
+    sequelize.query(`${query}`, { model: tbProvider }
+    ).then(res => {
+        statusCode = 200;
+        response.success = true;
+        response.data = res;
+    }).catch(err => {
+        response.error = err.message;
+    }).finally(() => {
+        return res.status(statusCode).json(response);
+    })
+};
+
 module.exports = {
     async create(req, res) {
-        const { name, logo, uf, kwhPrice, kwhMinLimit } = req.body;
+        const { name, logo, uf, kwhPrice, kwhMinLimit, totalClients, averageRating } = req.body;
         let response = { ...responseModel };
         let statusCode = 500;
 
         const isInvalidRequest = validateRequestFields(req, res, response);
         if (isInvalidRequest) return false;
         
-        sequelize.query(`INSERT INTO tbProvider VALUES ('${name}', '${logo}', '${uf}', ${kwhPrice}, ${kwhMinLimit})`, { model: tbProvider }
+        sequelize.query(`INSERT INTO tbProvider VALUES ('${name}', '${logo}', '${uf}', ${kwhPrice}, ${kwhMinLimit}, ${totalClients}, ${averageRating})`, { model: tbProvider }
         ).then(() => {
             statusCode = 201;
             response.success = true;
@@ -28,19 +44,13 @@ module.exports = {
         })
     },
 
-    async listAll(req, res) {
-        let response = { ...responseModel };
-        let statusCode = 500;
-        
-        sequelize.query(`SELECT * FROM tbProvider`, { model: tbProvider }
-        ).then(res => {
-            statusCode = 200;
-            response.success = true;
-            response.data = res;
-        }).catch(err => {
-            response.error = err.message;
-        }).finally(() => {
-            return res.status(statusCode).json(response);
-        })
+    async getAll(req, res) {
+        const query = 'SELECT * FROM tbProvider';
+        get(req, res, query);
     },
+
+    async getCompatibleProvider(req, res) {
+        const query = `SELECT * FROM tbProvider WHERE providerKwhLimit >= ${req.params.limit}`;
+        get(req, res, query);
+    }
 };
